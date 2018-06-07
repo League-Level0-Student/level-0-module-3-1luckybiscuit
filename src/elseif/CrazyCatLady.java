fddc111e74b48213e8a7dc93b89684b89281784d
1luckybiscuit <league@WTS10.attlocal.net>
2018-06-06 19:58:33 -0700

    Dot Race --> Crazy Cat Lady
    How many shrimps do you have to eat
    Before you make your skin turn pink
    Eat too much and you’ll get sick
    Shrimps are pretty rich
    👌👌👌👌👌👌👌👌

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,29 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+		String ask = JOptionPane.showInputDialog("What is the quantity of feline mammals currently inhabiting your household?");
 		// 2. Convert their answer into an int
-
+		int as = Integer.parseInt(ask);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+		if(as >= 3) {
+			JOptionPane.showMessageDialog(null, "Since you have a high amount of felines in your vicinity, I will now proceed to make fun of you and call you a \"crazy cat lady\".");
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+		else if(as < 3 && as > 0) {
+			playVideo("https://www.youtube.com/watch?v=JI7cIYcnTRU");
+		}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		else {
+			playVideo("https://www.youtube.com/watch?v=jAZZTE9t7g0");
+			JOptionPane.showInputDialog("How do you feel now?");
+			JOptionPane.showMessageDialog(null, "I don't care. Here's another:");
+			playVideo("https://www.youtube.com/watch?v=g-lFZObPxIU");
+		}
 	}
 
 	static void playVideo(String videoURL) {
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,55 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot owo = new Robot();
+		owo.setX(300);
+		owo.penDown();
+		owo.miniaturize();
+		owo.setPenWidth(20);
+		owo.setSpeed(300);
 		//3. Ask the user what color they would like the robot to draw
-		
+		for(int i = 0; i<20; i++) {
+		String ask = JOptionPane.showInputDialog("What color do you want to draw?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if(ask.equalsIgnoreCase("black")) {
+			owo.setPenColor(0,0,0);
+		}else if(ask.equalsIgnoreCase("red")) {
+			owo.setPenColor(255,0,0);
+		}else if(ask.equalsIgnoreCase("orange")) {
+			owo.setPenColor(255,128,0);
+		}else if(ask.equalsIgnoreCase("yellow")) {
+			owo.setPenColor(255,255,0);
+		}else if(ask.equalsIgnoreCase("green")) {
+			owo.setPenColor(0,255,0);
+		}else if(ask.equalsIgnoreCase("blue")) {
+			owo.setPenColor(0,0,255);
+		}else if(ask.equalsIgnoreCase("purple")) {
+			owo.setPenColor(200,0,255);
+		}else if(ask.equalsIgnoreCase("pink")) {
+			owo.setPenColor(255,0,200);
+		}else if(ask.equalsIgnoreCase("brown")) {
+			owo.setPenColor(50,0,50);
+		}else {
+			JOptionPane.showMessageDialog(null, "Undeterminable. Recalibrating...");
+			owo.setRandomPenColor();
+		}
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+			owo.move(50);
+			owo.turn(18);
 	}
-}
+		JOptionPane.showMessageDialog(null, "Finished! What a masterpiece!");
+	}}
+
